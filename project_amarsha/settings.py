312dd47e28c857ac99a9269e3948706e684e5a6f
thisisamal <amalpullan4@gmail.com>
2023-04-21 15:20:16 +0530

    added donation categoryres

--- project_amarsha/settings.py
+++ project_amarsha/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'amarshan',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'password',
         
     }
 }
