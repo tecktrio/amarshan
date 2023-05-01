986c42ec52a6724371e3524d7ae5dfacfd1f3f98
thisisamal <amalpullan4@gmail.com>
2023-05-01 18:41:31 +0530

    done

--- project_amarsha/settings.py
+++ project_amarsha/settings.py
@@ -96,6 +96,7 @@ DATABASES = DATABASES={
             'ENGINE':'django.db.backends.mysql',
             'NAME': 'amarshan',
             'USER': 'root',
+            
             'PASSWORD': 'password',
          }
         }
