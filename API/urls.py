16ba67b08947f3dacf6157e662c15f9eb094472c
thisisamal <amalpullan4@gmail.com>
2023-05-01 11:08:10 +0530

    done

--- API/urls.py
+++ API/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     
     # User
     path('donations/<int:id>', views.Donation_content.as_view()),
-    path('donations/<str:category>', views.Donation_content.as_view()),
+    path('donations/filter/<str:category>', views.Donation_content.as_view()),
     path('donations/upload', views.Upload.as_view()),
     path('donations/categories',views.Handle_Donation_categories.as_view()),
     path('donations/categories/<int:id>',views.Handle_Donation_categories.as_view()),
