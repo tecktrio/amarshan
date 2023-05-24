ff29efa9915c3cd7c5fb682f14e83b1aeab06598
thisisamal <amalpullan4@gmail.com>
2023-05-24 18:57:20 +0530

    back account updation1

--- API/admin.py
+++ API/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from API.models import Donations, Featured, Users
+from API.models import Donations, Featured, Users, Bank_Accounts
 from API.models import Storage
 
 # Register your models here.
@@ -8,3 +8,4 @@ admin.site.register(Users)
 admin.site.register(Donations)
 admin.site.register(Featured)
 admin.site.register(Storage)
+admin.site.register(Bank_Accounts)
