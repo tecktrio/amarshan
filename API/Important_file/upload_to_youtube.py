b24649a7d16c9d83d806873958ae33251c9937ff
widecity <widecity.official@gmail.com>
2023-08-23 22:24:05 +0530

    new instance

--- API/Important_file/upload_to_youtube.py
+++ API/Important_file/upload_to_youtube.py
@@ -28,17 +28,6 @@ RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError)
 # codes is raised.
 RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
-# the Google API Console at
-# https://console.cloud.google.com/.
-# Please ensure that you have enabled the YouTube Data API for your project.
-# For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
-# For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-
 #######################################################################################################
 #
 #       wanted to change the youtube credential, please find the file named youtube_secret.json
--- README.md
+++ README.md
@@ -1,22 +1,7 @@
 # AMARSHAN V1
-This project is a simple web application that allows users to create and manage tasks.
 
 ### Features
-Users can create tasks with a title and description
-Users can mark tasks as completed
-Users can delete tasks
 ### Installation
-Clone this repository
-Install dependencies: npm install
-Start the server: npm start
 ### Usage
-Open the web application in your browser at http://localhost:3000
-Create a new task by clicking the "Add Task" button
-Edit a task by clicking on its title or description
-Mark a task as completed by clicking the checkbox next to it
-Delete a task by clicking the "Delete" button next to it
 ### Contributing
-If you would like to contribute to this project, please fork the repository and submit a pull request.
-
 ### License
-This project is licensed under the MIT License. See the LICENSE file for more information.
